5b7d0d28f041662f726bb940a35ba24bca82254f
Yang Shuangcheng <yscljh@sina.com>
2019-12-20 22:29:49 +0800

    no guifont

--- bin/a.bat
+++ bin/a.bat
@@ -3,12 +3,6 @@
 ;;goto :eof
 ;; vim:ft=elisp:syntax=lisp:lisp
 (with-temp-file "tags.txt"
-  (insert "!_TAG_FILE_FORMAT	2	/extended format; --format=1 will not append ;\" to lines/\n")
-  (insert "!_TAG_FILE_SORTED	1	/0=unsorted, 1=sorted, 2=foldcase/\n")
-  (insert "!_TAG_PROGRAM_AUTHOR	Darren Hiebert	/dhiebert@users.sourceforge.net/\n")
-  (insert "!_TAG_PROGRAM_NAME	Exuberant Ctags	//\n")
-  (insert "!_TAG_PROGRAM_URL	http://ctags.sourceforge.net	/official site/\n")
-  (insert "!_TAG_PROGRAM_VERSION	5.8	//\n")
   (let ((symbols '()))
     (mapatoms (lambda (x) (push x symbols)))
     (setf symbols (sort symbols #'string< ))
---
+++ bin/a.el
@@ -0,0 +1,29 @@
+(load-file "/home/a/.vim/bin/elisptags")
+(defun fact (n)
+  (if (< n 2)
+      1
+    (* n (fact (- n 1)))))
+
+(fact 123)
+
+(require 'disass)
+
+
+(defun dis(fn)
+  "disassemble fn and print it"
+  (disassemble fn)
+  (with-current-buffer "*Disassemble*"
+    (buffer-string)))
+
+(dis 'dis)
+(dis 'fact)
+(help-function-arglist 'fact)
+(help-function-arglist 'defmacro)
+(help-function-arglist 'string-prefix-p)
+(documentation 'string-prefix-p)
+(elisp-get-fnsym-args-string 'string-prefix-p)
+(elisp-get-var-docstring 'exec-path)
+
+(elisp-get-fnsym-args-string 'elisp-get-fnsym-args-string)
+
+(elisptags)
